72868f8b3b00768b2053c2b7cfc6fe83c4689123
Diegomjx <55863604+Diegomjx@users.noreply.github.com>
2025-01-28 19:09:21 -0600

    Update FileManager.js

--- Scripts/FileManager.js
+++ Scripts/FileManager.js
@@ -1,8 +1,11 @@
+//Firmware: Rogue master
+//Not Aproved
+
 let storage = require("storage");
 let submenu = require("submenu");
 let dialog = require("dialog");
-//let infrared = require("infrared");
-//let subghz = require("subghz");
+let infrared = require("infrared");
+let subghz = require("subghz");
 
 
 let path = "/ext/payloads";
@@ -12,11 +15,13 @@ if (storage.exists(path)) {
         let result = dialog.pickFile(path, "*"); // Use "*" for second parameter to allow all files.
         if (storage.exists(result)) {
             if(result.indexOf(".js") > -1) {
-                print("File exists: is javascript");
+                print("Is a JS file");
             }else if(result.indexOf(".sub") > -1) {
-                print("File exists: is subghz");
+                let resultsubghz = subghz.transmitFile(result);
+                if (resultsubghz) { print("sent"); } else { print("failed to send"); }
             } else if(result.indexOf(".ir") > -1) {
-                print("File exists: is infrared");
+                let resultinfrared = infrared.transmitFile(result);
+                if (resultinfrared) { print("sent"); } else { print("failed to send"); }
             } else {
                 print("File exists: is unknown");
             }
